5d3915af1d91abe1710d1c891a25576e02296c1d
Lucas <walifedev@gmail.com>
2020-12-26 11:16:28 +0000

    Added dog gallery and improved mobile nav
    Made navigation font-size mobile friendly.
    
    Imported copyright free images from unsplash, and added them to the page using JavaScript. The only problem I am having is aligning them properly, I am considering using a table to align them.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const hamburger = document.querySelector('#hamburger');
 const navContainer = document.querySelector('#navBox');
+const gallery = document.querySelector('#gallery');
+const dogImg = './img/dog';
 
 hamburger.addEventListener('click', () => {
     navContainer.classList.toggle('showNav');
 });
+
+for(let i = 2; i <= 9; i++){
+    let galleryImg = document.createElement('img');
+    galleryImg.src = `${dogImg}${i}.jpg`;
+    gallery.appendChild(galleryImg);
+}
